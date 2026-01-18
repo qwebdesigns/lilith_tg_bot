44f7144fb5e9a6fda9ed917105c09100023f0185
Qweb Design <qwebdesing@inbox.ru>
2026-01-19 00:47:23 +0200

    123

--- commands/clans.py
+++ commands/clans.py
@@ -3,10 +3,15 @@ from services import get_text
 keys = ["клан", "клан вк", "клан стим", "клан херо"]
 
 PERMISSIONS = {
-    "клан": 'clan_all',
-    "клан вк": 'clan_vk',
-    "клан стим": 'clan_steam',
-    "клан херо": 'clan_hero'
+    "клан": "clan_all",
+    "клан стим": "clan_steam",
+    "клан херо": "clan_hero",
+    "клан вкплей": "clan_all",
+    "клан вк плей": "clan_all",
+    "клан вк": "clan_all",
+    "клан майл": "clan_all",
+    "клан окру": "clan_all",
+    "клан ок": "clan_all",
 }
 
 async def run(message, args, bot):
@@ -15,11 +20,15 @@ async def run(message, args, bot):
     resp = ""
     if "клан вк плей" in cmd:
         resp = await get_text("clan_vk_play.php")
+    elif "клан вкплей" in cmd:
+        resp = await get_text("clan_vk_play.php")
     elif "клан стим" in cmd:
         resp = await get_text("clan_steam.php")
     elif "клан херо" in cmd:
         resp = await get_text("clan_steam_t1.php")
     elif "клан окру" in cmd:
+        resp = await get_text("clan_okru.php")
+    elif "клан ок" in cmd:
         resp = await get_text("clan_okru.php")
     elif "клан вк" in cmd:
         resp = await get_text("clan.php")
